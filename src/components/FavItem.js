26e1a383e2912488e82033f00fd9f5dc240888e9
kazım <kazimnergiz123@gmail.com>
2023-02-04 16:30:14 +0300

    S10G4 tamamlandı

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeFav } from "../actions";
 
-function FavItem({ title }) {
+function FavItem({ title, id, key }) {
+  const dispatch = useDispatch();
+
+  const deleteFav = () => {
+    dispatch(removeFav(id));
+  };
+
   return (
-    <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
-      <div className="flex-1 pr-4">{title}</div>
+    <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition duration-300 ease-in-out">
+      <div className="flex-1 pr-4 text-lg font-bold">{title}</div>
       <button
-        onClick={() => { }}
-        className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
+        onClick={() => deleteFav(id)}
+        className="transition duration-300 ease-in-out px-3 py-2 text-sm rounded bg-red-500 hover:bg-red-700 text-white"
       >
-        Çıkar
+        Sil
       </button>
     </div>
   );
